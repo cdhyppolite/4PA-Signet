0d5a477340ba3589b47b14262990ee325569b03b
cdhyppolite <92059993+cdhyppolite@users.noreply.github.com>
2022-05-04 14:54:29 -0400

    voir les signets ajoutés

--- src/composants/Dossier.jsx
+++ src/composants/Dossier.jsx
@@ -1,17 +1,21 @@
-import './Dossier.scss'; 
+import './Dossier.scss';
 import IconButton from '@mui/material/IconButton';
 import SortIcon from '@mui/icons-material/Sort';
+import CloseIcon from '@mui/icons-material/Close';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import couvertureDefaut from '../images/couverture-defaut.webp';
 import { convertirDate } from '../code/helper';
-import { useState, useContext  } from 'react';
+import { useState, useContext } from 'react';
 import FrmDossier from './FrmDossier';
 import * as signetModele from '../code/signet-modele';
-import {UtilisateurContext} from './Appli';
+import { UtilisateurContext } from './Appli';
+import { ButtonUnstyled } from '@mui/base';
 
-export default function Dossier({id, titre, couleur, dateModif, couverture, supprimerDossier, modifierDossier, ajouterSignet, top3}) {
+export default function Dossier({ id, titre, couleur, dateModif, couverture, supprimerDossier, modifierDossier, ajouterSignet, top3 }) {
+  // État carte active
+  const[carteActive, setCarteActive] = useState(false);
   // Identifiant de l'utilisateur 
   const utilisateur = useContext(UtilisateurContext)
   const uid = utilisateur.uid;
@@ -25,29 +29,32 @@ export default function Dossier({id, titre, couleur, dateModif, couverture, supp
   // État du formulaire de modification
   const [ouvertFrm, setOuvertFrm] = useState(false);
 
-  function gererMenu(event) {
-    setEltAncrage(event.currentTarget);
+  function gererMenu(evt) {
+    setEltAncrage(evt.currentTarget);
+    evt.stopPropagation();
   };
 
-  function gererFermerMenu() {
-    
+  function gererFermerMenu(evt) {
     setEltAncrage(null);
+    evt.stopPropagation();
   };
 
-  function afficherFormulaireModifier() {
+  function afficherFormulaireModifier(evt) {
+    evt.stopPropagation();
     // Ouvrir le formulaire de modification du dossier (transférer l'info sir le
     // dossier dans le formulaire) ...
     setOuvertFrm(true);
     // ... puis fermer le menu.
-    gererFermerMenu();
+    gererFermerMenu(evt);
   }
-  
-  function gererSupprimer() {
+
+  function gererSupprimer(evt) {
+    evt.stopPropagation();
     // Appeler la fonction de ListeDossiers qui gère la suppression dans Firestore
     supprimerDossier(id);
 
     // ... puis fermer le menu.
-    gererFermerMenu();
+    gererFermerMenu(evt);
   }
 
   // Tester si l'URL dans la variable couverture est valide
@@ -55,7 +62,7 @@ export default function Dossier({id, titre, couleur, dateModif, couverture, supp
   try {
     urlCouverture = new URL(couverture);
   }
-  catch(e) {
+  catch (e) {
     couverture = couvertureDefaut;
   }
   // État dropzone
@@ -69,8 +76,20 @@ export default function Dossier({id, titre, couleur, dateModif, couverture, supp
     evt.preventDefault();
     setDropzone(false);
     let url = evt.dataTransfer.getData("URL");
-    if (url=='') {url='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSESQdWf_4jX4ECxu40tOY5gMGll0IdxV69lIreQKwuUI-5Z8_77UJw6fiToSM2XWLHimM&usqp=CAU'}
-    ajouterSignet(id, url);
+    // Récupérer le titre
+
+    /*fetch("https://cors-anywhere.herokuapp.com/corsdemo/" + url)
+    .then(reponse => reponse.text())
+    .then(
+      chaineDoc => {
+        const doc = new DOMParser().parseFromString(chaineDoc,"text/html");
+        // const titre = doc.querySelectorAll('title')[0].innerText;
+        // console.log(titre);
+        if (url == '') { url = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSESQdWf_4jX4ECxu40tOY5gMGll0IdxV69lIreQKwuUI-5Z8_77UJw6fiToSM2XWLHimM&usqp=CAU' }
+      }
+    );*/
+    ajouterSignet(id, url, "Avenir");
+    
   }
 
   function gererDragEnter(evt) {
@@ -88,9 +107,9 @@ export default function Dossier({id, titre, couleur, dateModif, couverture, supp
     modifierDossier(id, titre, url, couleur);
   }
 
-  function ajouterSignet(idDossier, url) {
-    if (url=='') {url='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSESQdWf_4jX4ECxu40tOY5gMGll0IdxV69lIreQKwuUI-5Z8_77UJw6fiToSM2XWLHimM&usqp=CAU'}
-    const derniers3 = [...signets, {url: url, titre: 'z'}].slice(-3);
+  function ajouterSignet(idDossier, url, titre) {
+    if (url == '') { url = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSESQdWf_4jX4ECxu40tOY5gMGll0IdxV69lIreQKwuUI-5Z8_77UJw6fiToSM2XWLHimM&usqp=CAU' }
+    const derniers3 = [...signets, { url: url, titre: titre }].slice(-3);
     console.log(idDossier + ' ' + url);
     signetModele.creer(uid, idDossier, derniers3).then(
       () => setSignets(derniers3)
@@ -100,38 +119,52 @@ export default function Dossier({id, titre, couleur, dateModif, couverture, supp
   return (
     // Remarquez l'objet JS donné à la valeur de l'attribut style en JSX, voir : 
     // https://reactjs.org/docs/dom-elements.html#style
-    <article className={'Dossier ' +(dropzone ? 'dropzone' : '')} onDrop={gererDrop} onDragOver={gererDragOver} onDragEnter={gererDragEnter} onDragLeave={gererDragLeave} style={{backgroundColor: couleur}}>
-      <div className="couverture">
-        <IconButton className="deplacer" aria-label="déplacer" disableRipple={true}>
-          <SortIcon />
-        </IconButton>
-        <img className={(dropzone ? 'dropzone' : '')} /*onDrop={gererDropImg}*/ src={couverture || couvertureDefaut} alt={titre}/>
+    <article className={'Dossier ' + (dropzone ? 'dropzone' : '') + (carteActive ? 'actif' : '')} onDrop={gererDrop} onDragOver={gererDragOver} onDragEnter={gererDragEnter} onDragLeave={gererDragLeave} style={{ backgroundColor: couleur }}>
+      <div className="carte">
+        <div className="endroit" onClick={() => setCarteActive(true)}>
+          <div className="couverture">
+            <IconButton className="deplacer" aria-label="déplacer" disableRipple={true}>
+              <SortIcon />
+            </IconButton>
+            <img className={(dropzone ? 'dropzone' : '')} /*onDrop={gererDropImg}*/ src={couverture || couvertureDefaut} alt={titre} />
+          </div>
+          <div className="info">
+            <h2>{titre}</h2>
+            <p>Modifié : {convertirDate(dateModif.seconds)}</p>
+          </div>
+          <IconButton onClick={gererMenu} className="modifier" aria-label="modifier" size="small">
+            <MoreVertIcon />
+          </IconButton>
+          <Menu
+            id="menu-contextuel-dossier"
+            anchorEl={eltAncrage}
+            open={ouvertMenu}
+            onClose={gererFermerMenu}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'right',
+            }}
+            transformOrigin={{
+              vertical: 'bottom',
+              horizontal: 'right',
+            }}
+          >
+            <MenuItem onClick={afficherFormulaireModifier}>Modifier</MenuItem>
+            <MenuItem onClick={gererSupprimer}>Supprimer</MenuItem>
+          </Menu>
+        </div>
+        <div className="envers">
+          <ButtonUnstyled className="touner-carte" onClick={() => setCarteActive(false)}>
+            <CloseIcon/>
+          </ButtonUnstyled>
+          {
+            signets.map(
+              (signet, position) => <a key={position} href={signet.url} target="_blank">{signet.titre}</a>
+            )
+          }
+        </div>
       </div>
-      <div className="info">
-        <h2>{titre}</h2>
-        <p>Modifié : {convertirDate(dateModif.seconds)}</p>
-      </div>
-      <IconButton onClick={gererMenu} className="modifier" aria-label="modifier" size="small">
-        <MoreVertIcon />
-      </IconButton>
-      <Menu
-        id="menu-contextuel-dossier"
-        anchorEl={eltAncrage}
-        open={ouvertMenu}
-        onClose={gererFermerMenu}
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'right',
-        }}
-        transformOrigin={{
-          vertical: 'bottom',
-          horizontal: 'right',
-        }}
-      >
-        <MenuItem onClick={afficherFormulaireModifier}>Modifier</MenuItem>
-        <MenuItem onClick={gererSupprimer}>Supprimer</MenuItem>
-      </Menu>
-      <FrmDossier gererActionDossier={modifierDossier} ouvert={ouvertFrm} setOuvert={setOuvertFrm} id={id} titre_p={titre} couleur_p={couleur} couverture_p={couverture} action={'Modifier'}/>
+      <FrmDossier gererActionDossier={modifierDossier} ouvert={ouvertFrm} setOuvert={setOuvertFrm} id={id} titre_p={titre} couleur_p={couleur} couverture_p={couverture} action={'Modifier'} />
     </article>
   );
 }
